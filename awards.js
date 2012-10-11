52bb24bd787abc35d696d840cbf3981f0e598650
aboutaaron <aaron.colby.williams@gmail.com>
2012-10-10 17:25:19 -0700

    added LAT style.css, started front-end work

--- awards.js
+++ awards.js
@@ -34,10 +34,10 @@ jQuery(document).ready(function () {
 
       // If the difference in days from the event is greater than or equal to 0(today), Add it to the table
       if(awardShowDate.diff(today, 'days') > 0 ) {
-        show_td = jQuery('<li>' + show.name + ' | ' + awardShowDate.format('MMM DD, YYYY') + '</li>')
+        show_td = jQuery('<li><strong>' + show.name + '</strong> | ' + awardShowDate.format('MMM DD, YYYY') + '</li>')
           .appendTo("#showList")
       } else if(awardShowDate.diff(today, 'days') == 0) {
-          show_td = jQuery('<li>' + show.name + ' | ' + awardShowDate.calendar() + '</li>')
+          show_td = jQuery('<li><strong>' + show.name + '</strong> | ' + awardShowDate.calendar() + '</li>')
           .appendTo("#showList")
       } else {
         // Else, do nothing and console.log so I know it was evaluated.
